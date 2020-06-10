e4d34c3fcfba94f83f715b206be8b4f081299601
krcondolo <kevincondolo@gmail.com>
2020-06-10 02:40:34 -0500

    “prueba”

---
+++ funcionJs.js
@@ -0,0 +1,13 @@
+
+function mostrar(){
+	document.getElementById('mostrarOcultar').style.display="block";
+	document.getElementById('botonMostrar').style.display="none";	
+	document.getElementById('botonOcultar').style.display="block";	
+}
+
+function ocultar(){
+	document.getElementById('mostrarOcultar').style.display="none";
+	document.getElementById('botonOcultar').style.display="none";
+	document.getElementById('botonMostrar').style.display="block";	
+		
+}
